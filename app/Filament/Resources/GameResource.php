8c59254d9178e5a31c37972906ba8232b5ae0d39
Henry Lee <henryleeworld@gmail.com>
2023-08-24 00:04:25 +0800

    feat:初始化

---
+++ app/Filament/Resources/GameResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Models\Game;
+use Filament\Tables;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use App\Filament\Resources\GameResource\Pages;
+
+class GameResource extends Resource
+{
+    protected static ?string $model = Game::class;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label(__('Name'))
+                    ->required(),
+            ]);
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('game');
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Game');
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'   => Pages\ListGames::route('/'),
+            'create'  => Pages\CreateGame::route('/create'),
+            'edit'    => Pages\EditGame::route('/{record}/edit'),
+            'winners' => Pages\GameWinners::route('/{record}/winners'),
+        ];
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__('Name')),
+            ])
+            ->prependActions([
+                Tables\Actions\Action::make('Pick winners')
+                    ->label(__('Pick winners'))
+                    ->color('success')
+                    ->url(fn(Game $record): string => self::getUrl('winners', ['record' => $record]))
+            ]);
+    }
+}
---
+++ database/factories/GameFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Game;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class GameFactory extends Factory
+{
+    protected $model = Game::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(random_int(3, 5), true),
+        ];
+    }
+}
